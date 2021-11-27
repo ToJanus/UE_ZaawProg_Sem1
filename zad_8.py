941e29ac73d98693c59f9123709018b6b1dd6517
Tomasz Janus <tomajan160@student.polsl.pl>
2021-11-27 14:41:06 +0100

    Dokonczenie zad_8 z lab_3

--- zad_8.py
+++ zad_8.py
@@ -27,15 +27,18 @@ class Brewery:
                f"Street: {self.street}, City: {self.city}, State: {self.state}, Code: {self.postal_code}"
 
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--city', type=str)
-#
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('--city', type=str, default=None)
+args = parser.parse_args()
 
-payload = {'per_page': 20}
+city = str(args.city).replace(' ', '_')
+if args.city is None:
+    payload = {'per_page': 20}
+else:
+    payload = {'per_page': 20, 'by_city': city}
+
 response = requests.get('https://api.openbrewerydb.org/breweries', params=payload)
 
 list_of_breweries = [Brewery(x) for x in response.json()]
-
 for x in list_of_breweries:
     print(x)
