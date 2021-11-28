9d097ff0cd69a1615b51680e77b48284490c6d2c
Tomasz Janus <tomasz.janus@edu.uekat.pl>
2021-11-28 09:27:29 +0100

    Zmiana w lab_4 do sprawdzenia dzialania workflow

--- zad_1.py
+++ zad_1.py
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     student1 = Student('Jan', 51)
     student2 = Student('Marek', 50)
     print(student1.is_passed())
-    print(student2.is_passed())
+    print(student2.is_passed()) # Small change to check if workflow works
